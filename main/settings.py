b133198306e89906bf6b8e05048645aa65f2345d
Uoyroem <Uoyroem@gmail.com>
2022-12-12 12:09:26 +0600

    user_profile to users.

--- main/settings.py
+++ main/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop',
-    'user_profile'
+    'users'
 ]
 
 MIDDLEWARE = [
--- shop/models.py
+++ shop/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-from user_profile.models import Profile
+from users.models import Profile
 import re
 
 
