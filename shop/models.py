933a0ba91fe4e7fc53a7dedae5c03f9f0c785e38
Uoyroem <Uoyroem@gmail.com>
2022-12-21 22:17:38 +0600

    Product detail

--- shop/models.py
+++ shop/models.py
@@ -9,45 +9,56 @@ class Product(models.Model):
     price = models.FloatField()
     category = models.ForeignKey('shop.Category', on_delete=models.CASCADE)
     brand = models.ForeignKey('shop.Brand', on_delete=models.CASCADE)
-    favorite = models.ManyToManyField(Profile, related_name='favorite_products', blank=True)
-    compare = models.ManyToManyField(Profile, related_name='compare_products', blank=True)
+    favorite = models.ManyToManyField(
+        Profile, related_name='favorite_products', blank=True)
+    compare = models.ManyToManyField(
+        Profile, related_name='compare_products', blank=True)
     description = models.TextField(null=True, blank=True)
-    specifications = models.JSONField(null=True, blank=True)
-    image_url = models.URLField()
-    
+    properties = models.JSONField(null=True, blank=True)
+
+    def first_image(self):
+        return self.images()[0]
+
+    def after_first_image(self):
+        return self.images()[1:]
+
+    def images(self) -> list[str]:
+        return self.properties['images']
+
     def to_favorite_url(self) -> str:
         return reverse('to_favorite', kwargs={'pk': self.id})
-    
+
     def to_compare_url(self) -> str:
         return reverse('to_compare', kwargs={'pk': self.id})
-    
+
     def get_price(self) -> str:
         return ' '.join(re.findall(r'\d{1,3}', str(round(self.price))[::-1]))[::-1] + ' ₸'
-    
+
     def __str__(self) -> str:
         return self.name
 
     def get_absolute_url(self):
         return reverse("product_detail", kwargs={"pk": self.id})
-    
+
     def bread_crumps(self):
         return self.category.bread_crumps()
-    
+
 
 class Category(models.Model):
     name = models.CharField(max_length=256)
-    parent = models.ForeignKey('shop.Category', on_delete=models.CASCADE, null=True, blank=True)
-    
+    parent = models.ForeignKey(
+        'shop.Category', on_delete=models.CASCADE, null=True, blank=True)
+
     def bread_crumps(self):
         divider = '<span class="user-select-none me-3 ms-3">/</span>'
         html = '<a class="lnk" href="{% url \'index\' %}">Главная</a>'
         html += divider
         html += f'<a class="lnk" href="{self.get_absolute_url()}">{self}</a>'
         return html
-    
+
     def __str__(self) -> str:
         return self.name
-    
+
     def get_absolute_url(self):
         return "category_detail"
 
@@ -55,16 +66,18 @@ class Category(models.Model):
 class Brand(models.Model):
     name = models.CharField(max_length=256)
     image_url = models.URLField()
-    
+
     def __str__(self) -> str:
         return self.name
-    
-    
+
+
 class Review(models.Model):
-    sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='product_reviews', null=True, blank=True)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews', null=True, blank=True)
+    sender = models.ForeignKey(Profile, on_delete=models.CASCADE,
+                               related_name='product_reviews', null=True, blank=True)
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name='reviews', null=True, blank=True)
     rating = models.IntegerField()
     review_text = models.TextField()
-    
+
     def __str__(self) -> str:
         return super().__str__()
--- shop/views.py
+++ shop/views.py
@@ -6,16 +6,17 @@ from . import models
 
 class IndexView(View):
     def get(self, request: HttpRequest, *args, **kwargs):
-        phone_category = models.Category.objects.filter(name='Смартфоны и планшеты').first()
+        phone_category = models.Category.objects.filter(
+            name='Смартфоны и планшеты').first()
         phone_list = models.Product.objects.filter(category=phone_category)
         return render(request, 'index.html', {
             'phone_list': phone_list
         })
-    
-    
+
+
 class ProductDetailView(DetailView):
     model = models.Product
-    
+
 
 def to_compare(request: HttpRequest, pk: int) -> HttpResponse:
     product = get_object_or_404(models.Product, id=pk)
@@ -30,34 +31,34 @@ def to_compare(request: HttpRequest, pk: int) -> HttpResponse:
 
 def to_favorities(request: HttpRequest, pk: int) -> HttpResponse:
     product = get_object_or_404(models.Product, id=pk)
-    
+
     if product.favorite.contains(request.user.profile):
         product.favorite.remove(request.user.profile)
     else:
         product.favorite.add(request.user.profile)
     return HttpResponse('Добавлен')
-            
-    
+
+
 class SearchView(View):
     def get(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
         query = request.GET['query']
         return render(request, 'search.html', {
             'query': query
         })
-        
+
 
 class CartView(View):
     def get(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
-        
+
         return render(request, 'cart.html')
-    
-    
+
+
 class ComparesView(View):
     def get(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
         return render(request, 'compares.html', {
             'compare_list': request.user.profile.compare_products.all()
         })
-    
+
 
 class FavoritiesView(View):
     def get(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
