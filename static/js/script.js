48f2b11a73dc889a6deaf2836231831cb7b97088
Uoyroem <Uoyroem@gmail.com>
2022-12-23 08:11:02 +0600

    Button styles toggle and counter for favorities

--- static/js/script.js
+++ static/js/script.js
@@ -7,7 +7,6 @@ function makeMessage(text, pasteElement, delay = 5000) {
     pasteElement.appendChild(message);
     setTimeout(() => {
         pasteElement.removeChild(message);
-        location.reload();
     }, delay);
 }
 
@@ -19,10 +18,35 @@ document.addEventListener('DOMContentLoaded', () => {
             fetch(this.dataset.to).then(response => response.text()).then(message => {
                 if (message) {
                     makeMessage(message, messages);
-                } else {
-                    location.reload();
                 }
-                
+                const counter = document.getElementById(this.dataset.forCounter);
+                const buttonIcon = this.querySelector('.bi');
+                let counted = false;
+                for (const toggle of this.dataset.toggle.split(',')) {
+                    const [a, b] = toggle.split('<->').map(item => item.trim());
+                    if (b) {
+                        if (buttonIcon.classList.contains(b)) {
+                            buttonIcon.classList.remove(b);
+                            buttonIcon.classList.add(a);
+                        } else {
+                            buttonIcon.classList.remove(a);
+                            buttonIcon.classList.add(b);
+                        }
+                        if (counter && !counted) {
+                            let count = parseInt(counter.textContent);
+                            counted = true;
+                            count += buttonIcon.classList.contains(b) ? 1 : -1;
+                            counter.textContent = count;
+                            counter.style.display = !count ? 'none' : 'inline';
+                        }
+                        continue;
+                    }
+                    buttonIcon.classList.toggle(a);
+                    if (counter && !counted) {
+                        counted = true
+                        counter.textContent = parseInt(counter.textContent) + buttonIcon.classList.contains(a) ? 1 : -1;
+                    }
+                }
             });
         });
     }
