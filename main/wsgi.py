a236e9c001985909b081ff0c7d2ceb0270ddb912
Uoyroem <Uoyroem@gmail.com>
2023-01-26 21:03:44 +0600

    d

--- main/wsgi.py
+++ main/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
-"""
-
-import os
-
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 from django.core.wsgi import get_wsgi_application
+import os
+import sys
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
-
-application = get_wsgi_application()
+path = os.path.expanduser('~/mysite')
+if path not in sys.path:
+    sys.path.insert(0, path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+application = StaticFilesHandler(get_wsgi_application())
