a61085d58e1af1f5ce40498f8ad8ecc4d186b73d
Uoyroem <Uoyroem@gmail.com>
2023-01-28 15:08:45 +0600

    Adaptive header

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,18 @@
-async function onButtonClick() {
-    to = this.dataset.to;
-    const response = await fetch(to);
-    const message = await response.text();
-    location.reload();
-}
 
 document.addEventListener('DOMContentLoaded', () => {
-    const buttons = document.querySelectorAll('button[data-to]');
-    if (!buttons) return;
-    buttons.forEach(button => button.addEventListener('click', onButtonClick));
+  const menuButton = document.querySelector('.button.button--large.navigation__menu-button');
+  const menuOpenIcon = document.querySelector('#catalog-menu-open-button-icon');
+  const menuCloseIcon = document.querySelector('#catalog-menu-close-button-icon');
+  const menuActiveCheckbox = document.querySelector('#catalog-active-checkbox');
+  menuOpenIcon.style.display = 'inline';
+  menuCloseIcon.style.display = 'none';
+  menuButton.addEventListener('', () => {
+    if (!menuActiveCheckbox.checked) {
+      menuOpenIcon.style.display = 'none';
+      menuCloseIcon.style.display = 'inline';
+    } else {
+      menuOpenIcon.style.display = 'inline';
+      menuCloseIcon.style.display = 'none';
+    }
+  });
 });
-
