1f24f7b3aa387f149e3fa4e03a06bc7d1e92505a
Uoyroem <Uoyroem@gmail.com>
2023-01-29 19:52:48 +0600

    Cart

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,34 @@
 /// <reference types="jquery"/>
 
+const METHODS = {
+  hideIf0(data, target, updater) {
+    target.text(data.totalCount);
+    if (!data.totalCount) target.addClass('hidden')
+    else target.removeClass('hidden');
+
+    const icon = updater.find('.icon');
+    if (data.count) {
+      icon.removeClass('icon--gray');
+      icon.addClass('icon--blue');
+    } else {
+      icon.removeClass('icon--blue');
+      icon.addClass('icon--gray');
+    }
+  },
+  productCard(data, target, updater) {
+    target.text(data.totalCount);
+    if (!data.totalCount) target.addClass('hidden')
+    else target.removeClass('hidden');
+
+    if (data.count) {
+      if (target.find('.icon').text() == 'shopping_cart') {
+        
+      }
+    }
+  }
+};
+
+
 $(function() {
   $('#catalog-active-checkbox').on('change', function() {
     const icon = $('#catalog-menu-open-button-icon, #catalog-menu-close-button-icon');
@@ -9,24 +38,14 @@ $(function() {
   $('[data-count-updater][data-count-updater-url]').on('click', function() {
     $.ajax({
       url: $(this).data('countUpdaterUrl'),
-    }).done(({message, count, totalCount}) => {
-      const target = $(`[data-update-count="${$(this).data('countUpdater')}"]`);
-      const icon = $(this).find('.icon');
-      console.log(message, count, totalCount)
-      if (!count) {
-        icon.removeClass('icon--blue');
-        icon.addClass('icon--gray');
+    }).done(data => {
+      const target = $(`[data-update-count="${$(this).data('countUpdater')}"][data-update-count-method]`); 
+      const methodName = target.data('updateCountMethod');
+      if (!(methodName in METHODS)) {
+        console.error(`${methodName} not in ${Object.keys(METHODS)}.`)
+        return;
       }
-      else {
-        icon.removeClass('icon--gray');
-        icon.addClass('icon--blue');
-      }
-      target.text(totalCount);
-      if (!totalCount) {
-        target.addClass('hidden');
-      } else {
-        target.removeClass('hidden');
-      }
+      METHODS[methodName](data, target, $(this));
     });
   });
 });
