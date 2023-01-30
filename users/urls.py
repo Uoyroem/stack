bc5fefa311c840e3f9083b473be7f804786ea130
Uoyroem <Uoyroem@gmail.com>
2023-01-30 17:49:34 +0600

    Login template

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('profile', views.ProfileView.as_view(), name='profile'),
-    path('users', include('django.contrib.auth.urls'), name='users'),
+    path('users/', include('django.contrib.auth.urls'), name='users'),
     path('signup', views.SignupView.as_view(), name='signup'),
     path('signup/register', views.register, name='register'),
     path('logout', views.logout, name='logout')
